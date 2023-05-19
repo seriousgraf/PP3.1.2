2c746ad4438d11651661d964dff0413f247db9ef
seriousgraf <grafnotstupid@gmail.com>
2023-05-19 16:00:07 +0300

    Initial commit

--- src/main/java/ru/crudtoboot/springboot/repositories/UserRepositoryImpl.java
+++ src/main/java/ru/crudtoboot/springboot/repositories/UserRepositoryImpl.java
@@ -1,6 +1,7 @@
 package ru.crudtoboot.springboot.repositories;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.crudtoboot.springboot.model.User;
 
 import javax.persistence.EntityManager;
@@ -15,27 +16,34 @@ public class UserRepositoryImpl implements UserRepository {
 
 
     @Override
+    @Transactional
     public void addUser(User user) {
         em.persist(user);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public void deleteUser(Long id) {
         User user = em.find(User.class, id);
         em.remove(user);
     }
 
     @Override
+    @Transactional
+
     public void updateUser(User user) {
         em.merge(user);
     }
 
     @Override
+    @Transactional
+
     public List<User> getAllUser() {
         return em.createQuery("FROM User", User.class).getResultList();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User getById(Long id) {
         return em.find(User.class, id);
     }
